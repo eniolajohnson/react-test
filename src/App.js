6070224804b1b85f56342bf57c6f38c4c2fa36e4
eniolajohnson <writingincodes@gmail.com>
2021-09-23 22:54:07 +0100

    completed star rating

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import starUnfilled from "./img/star-unfilled.png";
+import starFilled from "./img/starFilled.png";
+
+let arr = [1, 2, 3, 4, 5];
 
 function App() {
-  const test1 = "hello test";
-  return <h2>{test1}</h2>
+  const [hoverRating, setHoverRating] = useState(0);
+  const [rating, setRating] = useState(0);
+
+  const handleHoverOut = (e) => {
+    setHoverRating(0);
+  }
+
+  return (
+    <section>
+      {arr.map((arrItem) => {
+        console.log({arrItem, rating})
+        return <img key={arrItem-1} onMouseEnter={() => setHoverRating(arrItem)} onClick={() => setRating(arrItem)}  onMouseLeave={handleHoverOut} src={(arrItem <= hoverRating) || (arrItem <= rating) ? starFilled : starUnfilled}></img>
+      })}
+      <div>
+      </div>
+    </section>
+  )
 }
+
 
 export default App
